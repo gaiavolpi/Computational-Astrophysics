6a506e18a42b94dd78c06ba00b8073b060a8c3ec
Giacomo Menegatti <giacomo.menegatti@studenti.unipd>
2025-01-06 10:30:03 +0100

    now the package can be launched by command line alone

--- daneel_package/src/daneel/__main__.py
+++ daneel_package/src/daneel/__main__.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 from .detection.transit import *
 from .parameters.parameters import Parameters
 
@@ -17,16 +16,22 @@ def main():
     parser.add_argument(
         "-t",
         "--transit",
+        # If no dest is indicated and action is store true, the flag does not expect a value
         action = 'store_true',
         required=False,
     )
 
     args = parser.parse_args()
+    print('\nReading input file:')
+    print(f'--- {args.input_file}\n')
     
+    # Read input file parameters and store them into a dictionary
     input_pars = Parameters(args.input_file).params
 
     if args.transit:
-        print(input_pars)
+
+        planet_name = input_pars['title']
+        print(f'Plotting transit of {planet_name}')
         #Plot transit
         plot_transit(input_pars)
         
